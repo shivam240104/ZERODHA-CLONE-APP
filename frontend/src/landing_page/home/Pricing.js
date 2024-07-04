3001e4fcaa7b2854505eaf5148b0ff6a4aab8b38
shivam240104 <mauryatanmay845@gmail.com>
2024-07-05 05:01:50 +0530

    creating education section

--- frontend/src/landing_page/home/Pricing.js
+++ frontend/src/landing_page/home/Pricing.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Pricing() {
     return ( 
-        <div className='container'>
+        <div className='container mb-5'>
             <div className='row'>
                 <div className='col-4'>
                     <h1 className='mb-3'>Unbeatable pricing</h1>
