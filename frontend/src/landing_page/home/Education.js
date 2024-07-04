4ed718b9588559c54b103422f7bb353acfc3b02e
shivam240104 <mauryatanmay845@gmail.com>
2024-07-05 05:00:50 +0530

    some changes in div class

--- frontend/src/landing_page/home/Education.js
+++ frontend/src/landing_page/home/Education.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 function Education() {
-    return ( <h1>Education</h1>
+    return (
+        <div className='container mt-5'>
+            <div className='row'>
+                <div className='col-6'>
+                    <img src='media/image/education.svg' style={{width:"90%"}}/>
+
+                </div>
+                <div className='col-6'>
+                    <h1 className='mb-3 fs-2'>Free and open market education</h1>
+                    <p>Varsity, the largest online stock market education book in the world<br/> covering everything from the basics to advanced trading.</p>
+                    
+                    <div >
+                    <a href=''  style={{textDecoration:"none"}}>Varsity   <i class="fa fa-long-arrow-right" aria-hidden="true"></i> </a>
+                    </div>
+                    <p className='mt-5'>TradingQ&A, the most active trading and investment community in <br/> India for all your market related queries.</p>
+                    
+                    <div >
+                    <a href='' style={{textDecoration:"none"}}>TradingQ&A    <i class="fa fa-long-arrow-right" aria-hidden="true"></i> </a>
+                    </div>
+                </div>
+            </div>
+        </div>
 
      );
 }
